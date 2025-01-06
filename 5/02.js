1115117b27090cc6464b0a6091379e943eb90bf1
Rishab Sharma <167351217+rishabsharma07@users.noreply.github.com>
2025-01-06 17:47:40 +0530

    Conversions

---
+++ 5/02.js
@@ -0,0 +1,27 @@
+let score= "33abc"
+console.log(typeof(score));
+
+let numScore=Number(score)
+console.log(typeof(numScore));
+console.log(numScore)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn="rishab";
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "rishab" => true
+
+let someNum=33
+console.log(typeof someNum);
+
+let stringSomeNum= String(someNum)
+console.log(stringSomeNum);
+console.log(typeof stringSomeNum);
