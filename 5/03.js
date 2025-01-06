d1e16b68be1c18c57853ee7774c817897b7efbaf
Rishab Sharma <167351217+rishabsharma07@users.noreply.github.com>
2025-01-06 17:55:31 +0530

    Operations

---
+++ 5/03.js
@@ -0,0 +1,31 @@
+//OPERATIONS
+
+let value=3
+let negValue= -value
+
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
+
+let str1= "hello"
+let str2= " rishab"
+
+let str3= str1 + str2 
+console.log(str3);
+
+
+console.log("1" + 2); // first element is string so it will consider all elements as string
+console.log(1 + "2"); // first element is number so it will evaluate it and will consider the str element as string
+console.log("1" + 2 + 2); // first element is string so it will consider all elements as string
+console.log(1 + 2 + "2"); //now first element is number so it will evaluate the expression and then considered string as string
+
+console.log(true);
+console.log(""); //false
+
+console.log(+true);
+console.log(+"");
