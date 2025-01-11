98f0f44024531aeae3ff94c2049eeabee498c116
Rishab Sharma <167351217+rishabsharma07@users.noreply.github.com>
2025-01-11 23:42:09 +0530

    while & do while

---
+++ 27/02.js
@@ -0,0 +1,24 @@
+//while
+let index = 0
+while (index<=10) {
+    console.log(`the value is ${index}`);
+    index++
+}
+
+
+let arr= ["flash","superman","spiderman"]
+
+let len= arr.length
+let i=0;
+while (i<=len) {
+    console.log(`${arr[i]}`);
+    i++
+}
+
+//do while
+
+let score=10
+do {
+    console.log(`The value of score is ${score}`);
+    score++
+} while (score<=20);
